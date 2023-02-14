c9b54d7aa13869bf2b18323070bdda12c6ed8e5d
arlene-perez <arlene.perez@nbcuni.com>
2023-02-14 00:34:34 -0800

    Add background color

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Carousel } from '@/components/Carousel';
 
 // import NavBar from '@/components/NavBar.js';
 
-const IMAGE_URLS = ['headerImage.jpg','image2.jpg','image3.jpg'];
+const IMAGE_URLS = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg','image5.jpg', 'image6.jpg', 'image7.jpg','image8.jpg','image9.jpg','image11.jpg','image13.jpg'];
 
 import Head from 'next/head'
 
@@ -20,6 +20,17 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <Carousel slides={IMAGE_URLS}/>
+        <div className={styles.homepageSection}>
+          <div className={styles.homepageNames}>
+          Christian & Arlene
+          </div>
+          <div className={styles.homepageText}>
+          Friday, October 13, 2023
+          </div>
+          <div className={styles.homepageText}>
+          New Orleans, LA
+          </div>
+        </div>
       </div>
     </div>
   )
