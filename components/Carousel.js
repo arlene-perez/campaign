a66cd8553913b3436b2a48e741b3d5fad8a4d9f9
arlene-perez <arlene.perez@nbcuni.com>
2023-02-26 22:58:48 -0800

    Test image singular

--- components/Carousel.js
+++ components/Carousel.js
@@ -4,6 +4,9 @@ import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image';
 
 import styles from '../styles/page.module.css';
+import IMAGE_ONE from '../public/image1.jpg';
+import IMAGE_TWO from '../public/imageSecond.jpg';
+import IMAGE_THREE from '../public/image3.jpg';
 
 // const autoplayOptions = {
 //   delay: 4000,
@@ -12,12 +15,36 @@ import styles from '../styles/page.module.css';
 
 export const Carousel = (props) => {
   const { slides } = props
-  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
 
   return (
     <div className={styles.embla} ref={emblaRef}>
       <div className={styles.embla__container}>
-      {slides.map((src, index) => {
+      <div className={styles.embla__slide} key="1">
+          <Image
+            src={IMAGE_ONE}
+            alt="Image One"
+            className={styles.embla__slide__img}
+            fill
+          />
+        </div>
+        <div className={styles.embla__slide} key="2">
+          <Image
+            src={IMAGE_TWO}
+            alt="Image Two"
+            className={styles.embla__slide__img}
+            fill
+          />
+        </div>
+        <div className={styles.embla__slide} key="3">
+          <Image
+            src={IMAGE_THREE}
+            alt="Image Three"
+            className={styles.embla__slide__img}
+            fill
+          />
+        </div>
+      {/* {slides.map((src, index) => {
         return (
         <div className={styles.embla__slide} key={index}>
           <Image
@@ -28,7 +55,7 @@ export const Carousel = (props) => {
             objectFit="contain"
           />
         </div>
-      )})}
+      )})} */}
       </div>
     </div>
   )
