0a08005c2f69c7118a71195bea412707686374b2
arlene-perez <arlene.perez@nbcuni.com>
2023-03-07 00:05:52 -0800

    Fix rsvp form

--- pages/rsvp.js
+++ pages/rsvp.js
@@ -11,7 +11,7 @@ const RsvpPage = () => {
     <div className={styles.pageOuter}>
       <div className={styles.rsvpPage}>
         <PageTitle title="RSVP Page"/>
-        <h3 className={styles.rsvpDescription}>Please select your name below to RSVP. Your total guest count was sent via email/text. Please feel free to reach out to Christian/Arlene if you have any questions! <br></br>We can't wait to celebrate with you all!</h3>
+        <h3 className={styles.rsvpDescription}>Please select your name below to RSVP. Your total guest count was sent via email/text. Please feel free to reach out to Christian/Arlene if you have any questions! <br/>We can't wait to celebrate with you all!</h3>
        <div className={styles.dropDownForm}>
         <Dropdown />
         </div>
