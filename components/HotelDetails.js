ce34eb4b00ceee1f02780072c2c3fa6d7b594dfc
arlene-perez <arlene.perez@nbcuni.com>
2023-03-13 20:26:40 -0700

    Fix prices

--- components/HotelDetails.js
+++ components/HotelDetails.js
@@ -4,18 +4,21 @@ import Image from 'next/image';
 
 const HOTEL_DETAILS = [
   {
+    priceHotel: "Best Western",
     title: "Best Western Plus St. Christopher Hotel New Orleans",
     imageSrc: "bestWestern.jpeg", 
     href: "https://www.bestwestern.com/en_US/book/hotels-in-new-orleans/best-western-plus-st-christopher-hotel/propertyCode.19095.html", 
     priceText: "Waiting on group rates. This is where the wedding party will be staying at."
   },
   {
+    priceHotel: "Sheraton",
     title: "Sheraton New Orleans Hotel",
     imageSrc: "sheraton.jpeg", 
     href: "https://www.marriott.com/en-us/hotels/msyis-sheraton-new-orleans-hotel/overview/", 
     priceText: "Approximately $260 per night including taxes"
   },
   {
+    priceHotel: "Maison Dupuy",
     title: "Maison Dupuy Hotel",
     imageSrc: "maisonDupuy.jpeg", 
     href: "https://maisondupuy.com/?gclid=CjwKCAiAlp2fBhBPEiwA2Q10D3U_F2_mjV2fCcW17KHKZvw5NbjAb5p1eJsLpUIdVuzldHZ38Q0PbBoC5ikQAvD_BwE&gclsrc=aw.ds", 
@@ -23,13 +26,13 @@ const HOTEL_DETAILS = [
   }
 ]
 
-const HotelDetails = ({title, imageSrc, href, priceText}) => {
+const HotelDetails = () => {
   return (
     <div className={styles.hotelSection}>
       {HOTEL_DETAILS.map((item, index) => {
         return (
           <div className={styles.hotel} key={index}>
-            <div className={styles.hotelPrice}>Price: {item.priceText}</div>
+            <div className={styles.hotelPrice}>{item.priceHotel} Price: {item.priceText}</div>
             <div className={styles.hotelLink}>
               <Link href={item.href}>
                 {item.title}
